990941884b28c3b5c92b2f1e57ba1d4eea37bd2c
BrockPaperScissors <michaeljbrock91@gmail.com>
2023-03-10 15:47:40 -0800

    setting up react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./App.css";
 import Welcome from "./Components/Welcome";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-	return <Welcome />;
+	return (
+		<>
+			<body>
+				<header id="homepage-header">
+					<h1>Team Deets 2</h1>
+				</header>
+			</body>
+			<Welcome />;
+		</>
+	);
 }
 
 export default App;
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -2,7 +2,7 @@ export default function Welcome() {
 	return (
 		<>
 			<h1> Welcome to Team Deets 2</h1>
-			<h2> A tool for League of Legends statistics</h2>
+			<h2> A tool to check out your League of Legends statistics</h2>
 		</>
 	);
 }
